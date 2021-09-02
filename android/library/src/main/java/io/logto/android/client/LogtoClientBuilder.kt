f46bdbb22d9fc1a232a8dc5e17b300c7471abdf2
Xiao Yijun <xiaoyijun@silverhand.io>
2021-09-02 17:50:04 +0800

    improvement: use gson fieldNamePolicy setting instead of handwriting

--- android/library/src/main/java/io/logto/android/client/LogtoClientBuilder.kt
+++ android/library/src/main/java/io/logto/android/client/LogtoClientBuilder.kt
@@ -1,15 +1,23 @@
 package io.logto.android.client
 
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
 import io.logto.android.LogtoConfig
 import io.logto.android.client.api.LogtoClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class LogtoClientBuilder(private val logtoConfig: LogtoConfig) {
-    fun build(): LogtoClient = Retrofit
-        .Builder()
-        .baseUrl(logtoConfig.oidcEndpoint)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-        .create(LogtoClient::class.java)
+    fun build(): LogtoClient {
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
+        return Retrofit
+            .Builder()
+            .baseUrl(logtoConfig.oidcEndpoint)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(LogtoClient::class.java)
+    }
 }
--- android/library/src/main/java/io/logto/android/model/Credential.kt
+++ android/library/src/main/java/io/logto/android/model/Credential.kt
@@ -1,12 +1,10 @@
 package io.logto.android.model
 
-import com.google.gson.annotations.SerializedName
-
 data class Credential(
-    @SerializedName("access_token") val accessToken: String,
-    @SerializedName("expires_in") val expiresIn: String,
-    @SerializedName("refresh_token") val refreshToken: String?,
-    @SerializedName("id_token") val idToken: String,
-    @SerializedName("scope") val scope: String,
-    @SerializedName("token_type") val tokenType: String,
+    val accessToken: String,
+    val expiresIn: String,
+    val refreshToken: String?,
+    val idToken: String,
+    val scope: String,
+    val tokenType: String,
 )
