1b75dc3a71834abb805baab1acd0fe4a15f64de4
Xiao Yijun <xiaoyijun@silverhand.io>
2021-09-10 11:14:04 +0800

    feat: store credentials (#10)
    * feat: store credentials
    
    * refactor: avoid redundant storage key check
    
    * chore: make credential storage optional for future config

--- android/library/src/main/java/io/logto/android/Logto.kt
+++ android/library/src/main/java/io/logto/android/Logto.kt
@@ -1,9 +1,19 @@
 package io.logto.android
 
+import android.content.Context
 import io.logto.android.authflow.webview.WebViewAuthFlow
+import io.logto.android.model.Credential
+import io.logto.android.storage.CredentialStorage
 
 class Logto {
     companion object {
-        fun webViewAuthFlow() = WebViewAuthFlow()
+        private var credentialStorage: CredentialStorage? = null
+
+        fun webViewAuthFlow(context: Context): WebViewAuthFlow {
+            credentialStorage = CredentialStorage(context.applicationContext)
+            return WebViewAuthFlow(context, credentialStorage)
+        }
+
+        fun getCredential(): Credential? = credentialStorage?.getCredential()
     }
 }
