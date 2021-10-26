d079cb479fbf5d120cc11b317c37cdfbfba75b21
Xiao Yijun <xiaoyijun@silverhand.io>
2021-10-26 14:14:32 +0800

    improvement: validate logtoConfig

--- android/library/src/main/java/io/logto/android/config/LogtoConfig.kt
+++ android/library/src/main/java/io/logto/android/config/LogtoConfig.kt
@@ -9,4 +9,18 @@ data class LogtoConfig(
 ) {
     val encodedScopes: String
         get() = scopes.joinToString(" ")
+
+    private fun validate() {
+        require(logtoUrl.isNotEmpty()) { "LogtoConfig: logtoUrl should not be empty"}
+        require(clientId.isNotEmpty()) { "LogtoConfig: clientId should not be empty"}
+        require(scopes.isNotEmpty()) { "LogtoConfig: scope list should not be empty" }
+        require(redirectUri.isNotEmpty()) { "LogtoConfig: redirectUri should not be empty" }
+        require(postLogoutRedirectUri.isNotEmpty()) {
+            "LogtoConfig: postLogoutRedirectUri should not be empty"
+        }
+    }
+
+    init {
+        validate()
+    }
 }
