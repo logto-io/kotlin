874eed35060eaf619a95924ba88a1202b87aa3ed
Xiao Yijun <xiaoyijun@silverhand.io>
2021-10-29 17:58:19 +0800

    test: add UtilsTest

--- android/library/src/test/java/io/logto/android/utils/UtilsTest.kt
+++ android/library/src/test/java/io/logto/android/utils/UtilsTest.kt
@@ -1,12 +1,29 @@
 package io.logto.android.utils
 
 import com.google.common.truth.Truth.assertThat
+import io.logto.android.exception.LogtoException
+import org.jose4j.jwk.JsonWebKey
+import org.jose4j.jwk.JsonWebKeySet
+import org.jose4j.jwk.RsaJsonWebKey
+import org.jose4j.jwk.RsaJwkGenerator
+import org.jose4j.jws.AlgorithmIdentifiers
+import org.jose4j.jws.JsonWebSignature
+import org.jose4j.jwt.JwtClaims
+import org.junit.Assert.assertThrows
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 
 @RunWith(RobolectricTestRunner::class)
 class UtilsTest {
+
+    private val testIssuer = "testIssuer"
+    private val testAudience = "testAudience"
+    private val testSubject = "testSubject"
+    private val testRsaJsonWebKey = RsaJwkGenerator.generateJwk(2048).apply {
+        keyId = "rsa-json-web-key-id"
+    }
+
     @Test
     fun buildUriWithQueries() {
         val baseUrl = "logto.io"
@@ -25,5 +42,108 @@ class UtilsTest {
         val lifeTime = 1000L
         val expectedExpiresAt = 2000L
         assertThat(Utils.expiresAtFrom(startTime, lifeTime)).isEqualTo(expectedExpiresAt)
+    }
+
+    @Test
+    fun verifyIdTokenWithValidIdToken() {
+        val claims = JwtClaims().apply {
+            issuer = testIssuer
+            setAudience(testAudience)
+            subject = testSubject
+            setIssuedAtToNow()
+            setExpirationTimeMinutesInTheFuture(60F)
+            setGeneratedJwtId()
+        }
+
+        val idToken = generateIdToken(testRsaJsonWebKey, claims)
+
+        val jwks = createJwks(testRsaJsonWebKey)
+
+        var logtoException: LogtoException? = null
+
+        try {
+            Utils.verifyIdToken(idToken, testAudience, jwks)
+        } catch (exception: LogtoException) {
+            logtoException = exception
+        }
+
+        assertThat(logtoException).isNull()
+    }
+
+    @Test
+    fun verifyIdTokenMissingSubjectShouldThrow() {
+        val claims = JwtClaims().apply {
+            issuer = testIssuer
+            setAudience(testAudience)
+            setIssuedAtToNow()
+            setExpirationTimeMinutesInTheFuture(60F)
+            setGeneratedJwtId()
+        }
+        val idToken = generateIdToken(testRsaJsonWebKey, claims)
+        val jwks = createJwks(testRsaJsonWebKey)
+        assertThrows(LogtoException::class.java) {
+            Utils.verifyIdToken(idToken, testAudience, jwks)
+        }
+    }
+
+    @Test
+    fun verifyIdTokenMissingExpirationTimeShouldThrow() {
+        val claims = JwtClaims().apply {
+            issuer = testIssuer
+            setAudience(testAudience)
+            setIssuedAtToNow()
+            setExpirationTimeMinutesInTheFuture(60F)
+            setGeneratedJwtId()
+        }
+        val idToken = generateIdToken(testRsaJsonWebKey, claims)
+        val jwks = createJwks(testRsaJsonWebKey)
+        assertThrows(LogtoException::class.java) {
+            Utils.verifyIdToken(idToken, testAudience, jwks)
+        }
+    }
+
+    @Test
+    fun verifyIdTokenMissingIssuedAtShouldThrow() {
+        val claims = JwtClaims().apply {
+            issuer = testIssuer
+            setAudience(testAudience)
+            subject = testSubject
+            setExpirationTimeMinutesInTheFuture(60F)
+            setGeneratedJwtId()
+        }
+        val idToken = generateIdToken(testRsaJsonWebKey, claims)
+        val jwks = createJwks(testRsaJsonWebKey)
+        assertThrows(LogtoException::class.java) {
+            Utils.verifyIdToken(idToken, testAudience, jwks)
+        }
+    }
+
+    @Test
+    fun verifyIdTokenMissingAudienceShouldThrow() {
+        val claims = JwtClaims().apply {
+            issuer = testIssuer
+            subject = testSubject
+            setIssuedAtToNow()
+            setExpirationTimeMinutesInTheFuture(60F)
+            setGeneratedJwtId()
+        }
+        val idToken = generateIdToken(testRsaJsonWebKey, claims)
+        val jwks = createJwks(testRsaJsonWebKey)
+        assertThrows(LogtoException::class.java) {
+            Utils.verifyIdToken(idToken, testAudience, jwks)
+        }
+    }
+
+    private fun generateIdToken(rsaJsonWebKey: RsaJsonWebKey, claims: JwtClaims): String {
+        val jws = JsonWebSignature()
+        jws.payload = claims.toJson()
+        jws.key = rsaJsonWebKey.privateKey
+        jws.keyIdHeaderValue = rsaJsonWebKey.keyId
+        jws.algorithmHeaderValue = AlgorithmIdentifiers.RSA_USING_SHA256;
+        return jws.compactSerialization
+    }
+
+    private fun createJwks(jsonWebKey: JsonWebKey) = JsonWebKeySet().apply {
+        addJsonWebKey(jsonWebKey)
     }
 }
