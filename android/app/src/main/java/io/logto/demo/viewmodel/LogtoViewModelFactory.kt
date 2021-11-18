15b0d3715a11790ba6e69a76fece90627fbd9e7d
Xiao Yijun <xiaoyijun@silverhand.io>
2021-11-18 16:01:43 +0800

    feature: android SDK sample code (#46)
    * feature: android sdk sample code
    
    * refactor:  avoid using `onClick` on xml
    
    * refactor: rename `login / logout` to `sign in / out`
    
    * feature: logto state visualization
    
    * refactor: remove unnecessary code
    
    * feature: logto state visualization

---
+++ android/app/src/main/java/io/logto/demo/viewmodel/LogtoViewModelFactory.kt
@@ -0,0 +1,15 @@
+package io.logto.demo.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class LogtoViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LogtoViewModel::class.java)) {
+            return LogtoViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
