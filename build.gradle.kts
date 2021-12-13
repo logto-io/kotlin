048d92bb72b7e3550437b94e76e6701e1e8b4203
Xiao Yijun <xiaoyijun@silverhand.io>
2021-12-13 12:22:46 +0800

    feat: add android-sdk lint task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+tasks.register("androidSdkLint") {
+    dependsOn(gradle.includedBuild("android-sdk").task(":android-sdk:lint"))
+}
+
 tasks.register("test") {
     dependsOn(gradle.includedBuild("client").task(":test"))
     dependsOn(gradle.includedBuild("android-sdk").task(":android-sdk:test"))
