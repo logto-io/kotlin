e14128cd2b61e099e15819417177be99be432c08
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-17 22:09:42 +0800

    chore: detach old projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,3 @@
 tasks.register("checkCodeStyle") {
     dependsOn(gradle.includedBuild("kotlin").task(":detekt"))
 }
-
-tasks.register("lintAndroidSdk") {
-    dependsOn(gradle.includedBuild("android-sdk").task(":android-sdk:lint"))
-}
-
-tasks.register("test") {
-    dependsOn(gradle.includedBuild("client").task(":test"))
-    dependsOn(gradle.includedBuild("android-sdk").task(":android-sdk:test"))
-}
