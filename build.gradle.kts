9694f63d00f6a7a9a9dfa443cde538a861200790
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-18 10:02:27 +0800

    feat(core): testing env setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
 tasks.register("checkCodeStyle") {
     dependsOn(gradle.includedBuild("kotlin").task(":detekt"))
 }
+
+tasks.register("test") {
+    dependsOn(gradle.includedBuild("kotlin").task(":test"))
+}
--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -22,6 +22,17 @@ detekt {
     buildUponDefaultConfig = true
 }
 
+tasks {
+    test {
+        testLogging {
+            events("failed", "skipped", "passed", "standardOut", "standardError")
+            outputs.upToDateWhen { false }
+        }
+        reports.html.required.set(false)
+        reports.junitXml.required.set(false)
+    }
+}
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.5.31"))
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.9.3"))
@@ -30,4 +41,8 @@ dependencies {
     implementation("com.aventrix.jnanoid:jnanoid:2.0.0")
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.google.code.gson:gson:2.8.9")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("com.google.truth:truth:1.1.3")
 }
