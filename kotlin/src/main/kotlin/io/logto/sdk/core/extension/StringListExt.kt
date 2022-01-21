f78b30c67f934ff43c5b23de58849e3b9f59cf24
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-21 15:09:06 +0800

    refactor(core): ensureDefaultScopes extension

--- kotlin/src/main/kotlin/io/logto/sdk/core/extension/StringListExt.kt
+++ kotlin/src/main/kotlin/io/logto/sdk/core/extension/StringListExt.kt
@@ -2,20 +2,5 @@ package io.logto.sdk.core.extension
 
 import io.logto.sdk.core.constant.ReservedScope
 
-fun List<String>?.ensureDefaultScopes(): List<String> = this?.let {
-    if (it.contains(ReservedScope.OPENID) and it.contains(ReservedScope.OFFLINE_ACCESS)) {
-        return it
-    }
-
-    val mutableScopes = it.toMutableList()
-
-    if (!mutableScopes.contains(ReservedScope.OPENID)) {
-        mutableScopes.add(ReservedScope.OPENID)
-    }
-
-    if (!mutableScopes.contains(ReservedScope.OFFLINE_ACCESS)) {
-        mutableScopes.add(ReservedScope.OFFLINE_ACCESS)
-    }
-
-    return mutableScopes.toList()
-} ?: listOf(ReservedScope.OPENID, ReservedScope.OFFLINE_ACCESS)
+fun List<String>?.ensureDefaultScopes(): List<String> =
+    ((this ?: listOf()) + listOf(ReservedScope.OPENID, ReservedScope.OFFLINE_ACCESS)).distinct()
