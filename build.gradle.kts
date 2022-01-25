bcf19315ce1de3131af2d350d43d61e864fa493b
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-25 13:43:24 +0800

    feat(android): test & code style check ci setup (#84)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 tasks.register("clean") {
     dependsOn(gradle.includedBuild("kotlin").task(":clean"))
+    dependsOn(gradle.includedBuild("android").task(":logto-client:clean"))
 }
 
 tasks.register("checkCodeStyle") {
     dependsOn(gradle.includedBuild("kotlin").task(":detekt"))
+    dependsOn(gradle.includedBuild("android").task(":logto-client:detekt"))
 }
 
 tasks.register("test") {
     dependsOn(gradle.includedBuild("kotlin").task(":test"))
+    dependsOn(gradle.includedBuild("android").task(":logto-client:testDebugUnitTest"))
 }
