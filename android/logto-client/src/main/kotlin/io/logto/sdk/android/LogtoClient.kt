d7185d832b604d4c7a2ed954a60acf72747347d8
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-25 14:14:03 +0800

    feat(android): init logto client

--- android/logto-client/src/main/kotlin/io/logto/sdk/android/LogtoClient.kt
+++ android/logto-client/src/main/kotlin/io/logto/sdk/android/LogtoClient.kt
@@ -1,3 +1,19 @@
 package io.logto.sdk.android
 
-class LogtoClient
+import io.logto.sdk.android.type.AccessToken
+import io.logto.sdk.android.type.LogtoConfig
+import io.logto.sdk.core.type.OidcConfigResponse
+
+open class LogtoClient(
+    val logtoConfig: LogtoConfig
+) {
+    protected val accessTokenMap: Map<String, AccessToken> = mutableMapOf()
+
+    protected var refreshToken: String? = null
+
+    protected var idToken: String? = null
+
+    protected var oidcConfig: OidcConfigResponse? = null
+
+    fun isAuthenticated() = idToken != null
+}
