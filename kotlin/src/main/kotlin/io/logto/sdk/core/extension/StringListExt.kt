ed2593445238bc9b52a2508ac9aa66e9d5f43953
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-26 16:27:45 +0800

    refactor(core): refactor `StringListExt` to `ScopeUtils` (#88)

--- kotlin/src/main/kotlin/io/logto/sdk/core/extension/StringListExt.kt
+++
@@ -1,6 +0,0 @@
-package io.logto.sdk.core.extension
-
-import io.logto.sdk.core.constant.ReservedScope
-
-fun List<String>?.ensureDefaultScopes(): List<String> =
-    ((this ?: listOf()) + listOf(ReservedScope.OPENID, ReservedScope.OFFLINE_ACCESS)).distinct()
