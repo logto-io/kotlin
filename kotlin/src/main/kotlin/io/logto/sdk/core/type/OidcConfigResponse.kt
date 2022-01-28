12f4505646cbb36f9af3b0db410982f8c3a67f3e
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-28 13:08:26 +0800

    feat(core): add `userinfoEndpoint` to `OidcConfigResponse` (#96)

--- kotlin/src/main/kotlin/io/logto/sdk/core/type/OidcConfigResponse.kt
+++ kotlin/src/main/kotlin/io/logto/sdk/core/type/OidcConfigResponse.kt
@@ -4,6 +4,7 @@ data class OidcConfigResponse(
     val authorizationEndpoint: String,
     val tokenEndpoint: String,
     val endSessionEndpoint: String,
+    val userinfoEndpoint: String,
     val jwksUri: String,
     val issuer: String,
     val revocationEndpoint: String,
