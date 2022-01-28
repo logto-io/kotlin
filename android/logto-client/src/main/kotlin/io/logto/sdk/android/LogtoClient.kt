8e696469c8f2bef2ace2ba26b73abd01e46e0226
Xiao Yijun <xiaoyijun@silverhand.io>
2022-01-28 17:23:22 +0800

    feat(android): getAccessToken (#95)
    * feat(android): getAccessToken
    
    * refactor: check refreshToken where want to perform refresh
    
    * chore(android): fix grammar error in UTs
    
    * chore(android): add comments for `getAccessToken`
    
    * refactor(android): rename `makeAccessTokenKey` to `buildAccessTokenKey`
    
    * refactor(android): refreshToken
    
    * refactor(android): rename `refreshTokenCompletion` to `completion`

--- android/logto-client/src/main/kotlin/io/logto/sdk/android/LogtoClient.kt
+++ android/logto-client/src/main/kotlin/io/logto/sdk/android/LogtoClient.kt
@@ -1,17 +1,21 @@
 package io.logto.sdk.android
 
 import io.logto.sdk.android.callback.RetrieveCallback
+import io.logto.sdk.android.exception.LogtoException
 import io.logto.sdk.android.extension.oidcConfigEndpoint
 import io.logto.sdk.android.type.AccessToken
 import io.logto.sdk.android.type.LogtoConfig
+import io.logto.sdk.android.util.LogtoUtils
 import io.logto.sdk.core.Core
 import io.logto.sdk.core.http.HttpCompletion
 import io.logto.sdk.core.type.OidcConfigResponse
+import io.logto.sdk.core.type.RefreshTokenTokenResponse
+import org.jetbrains.annotations.TestOnly
 
 open class LogtoClient(
     val logtoConfig: LogtoConfig
 ) {
-    protected val accessTokenMap: Map<String, AccessToken> = mutableMapOf()
+    protected val accessTokenMap: MutableMap<String, AccessToken> = mutableMapOf()
 
     protected var refreshToken: String? = null
 
@@ -20,6 +24,109 @@ open class LogtoClient(
     protected var oidcConfig: OidcConfigResponse? = null
 
     fun isAuthenticated() = idToken != null
+
+    fun getAccessToken(callback: RetrieveCallback<AccessToken>) =
+        getAccessToken(null, null, callback)
+
+    fun getAccessToken(
+        resource: String?,
+        scope: List<String>?,
+        getAccessTokenCallback: RetrieveCallback<AccessToken>,
+    ) {
+        if (!isAuthenticated()) {
+            getAccessTokenCallback.onResult(LogtoException(LogtoException.Message.NOT_AUTHENTICATED), null)
+            return
+        }
+
+        resource?.let {
+            if (logtoConfig.resource?.contains(it) == false) {
+                getAccessTokenCallback.onResult(
+                    LogtoException(LogtoException.Message.RESOURCE_IS_NOT_GRANTED).apply { detail = it }, null
+                )
+                return
+            }
+        }
+
+        val finalScope = scope ?: logtoConfig.scope
+        if (!logtoConfig.scope.containsAll(finalScope)) {
+            getAccessTokenCallback.onResult(
+                LogtoException(LogtoException.Message.SCOPES_ARE_NOT_ALL_GRANTED).apply {
+                    detail = finalScope.toString()
+                },
+                null
+            )
+            return
+        }
+
+        // Retrieve access token from accessTokenMap
+        val accessTokenKey = buildAccessTokenKey(finalScope, resource)
+        val accessToken = accessTokenMap[accessTokenKey]
+        accessToken?.let {
+            if (it.expiresAt > LogtoUtils.nowRoundToSec()) {
+                getAccessTokenCallback.onResult(null, it)
+                return
+            }
+        }
+
+        // If no access token is valid, fetch a new token by refresh token
+        refreshToken(
+            resource = resource,
+            scope = finalScope,
+            completion = object : HttpCompletion<RefreshTokenTokenResponse> {
+                override fun onComplete(throwable: Throwable?, response: RefreshTokenTokenResponse?) {
+                    if (throwable != null) {
+                        getAccessTokenCallback.onResult(throwable, null)
+                        return
+                    }
+                    requireNotNull(response).let { tokenResponse ->
+                        val refreshedAccessToken = AccessToken(
+                            token = tokenResponse.accessToken,
+                            scope = tokenResponse.scope,
+                            expiresAt = LogtoUtils.expiresAtFrom(
+                                LogtoUtils.nowRoundToSec(),
+                                tokenResponse.expiresIn
+                            )
+                        )
+                        accessTokenMap[accessTokenKey] = refreshedAccessToken
+                        refreshToken = tokenResponse.refreshToken
+                        tokenResponse.idToken?.let { idToken = it }
+                        getAccessTokenCallback.onResult(null, refreshedAccessToken)
+                    }
+                }
+            }
+        )
+    }
+
+    private fun refreshToken(
+        resource: String?,
+        scope: List<String>?,
+        completion: HttpCompletion<RefreshTokenTokenResponse>,
+    ) {
+        if (refreshToken == null) {
+            completion.onComplete(
+                LogtoException(LogtoException.Message.MISSING_REFRESH_TOKEN), null
+            )
+            return
+        }
+        getOidcConfig(object : RetrieveCallback<OidcConfigResponse> {
+            override fun onResult(throwable: Throwable?, result: OidcConfigResponse?) {
+                if (throwable != null) {
+                    completion.onComplete(throwable, null)
+                    return
+                }
+                requireNotNull(result).let { oidcConfig ->
+                    Core.fetchTokenByRefreshToken(
+                        tokenEndpoint = oidcConfig.tokenEndpoint,
+                        clientId = logtoConfig.clientId,
+                        refreshToken = requireNotNull(refreshToken),
+                        resource = resource,
+                        scope = scope,
+                        completion = completion
+                    )
+                }
+            }
+        })
+    }
 
     internal fun getOidcConfig(callback: RetrieveCallback<OidcConfigResponse>) {
         if (oidcConfig != null) {
@@ -35,5 +142,18 @@ open class LogtoClient(
                 }
             }
         )
+    }
+
+    internal fun buildAccessTokenKey(scope: List<String>, resource: String?) =
+        "${scope.sorted().joinToString(" ")}@$resource"
+
+    @TestOnly
+    fun setupRefreshToken(token: String?) {
+        refreshToken = token
+    }
+
+    @TestOnly
+    fun setupAccessTokenMap(tokenMap: Map<String, AccessToken>) {
+        accessTokenMap.putAll(tokenMap)
     }
 }
