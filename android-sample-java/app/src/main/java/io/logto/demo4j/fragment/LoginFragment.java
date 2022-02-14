6c560482aa2a0513360cdc043172c160e099e5e4
Xiao Yijun <xiaoyijun@silverhand.io>
2022-02-14 13:58:47 +0800

    feat(android): java sample

---
+++ android-sample-java/app/src/main/java/io/logto/demo4j/fragment/LoginFragment.java
@@ -0,0 +1,41 @@
+package io.logto.demo4j.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import io.logto.demo4j.R;
+import io.logto.demo4j.viewmodel.LogtoViewModel;
+import io.logto.demo4j.viewmodel.LogtoViewModelFactory;
+
+public class LoginFragment extends Fragment {
+
+    private LogtoViewModel logtoViewModel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        initView(view);
+        initViewModel(view);
+        return view;
+    }
+
+    private void initView(View view) {
+        view.findViewById(R.id.sign_in_button).setOnClickListener( v -> {
+            logtoViewModel.signInWithBrowser(requireActivity());
+        });
+    }
+
+    private void initViewModel(View view) {
+        LogtoViewModelFactory logtoViewModelFactory = new LogtoViewModelFactory(requireActivity().getApplication());
+        logtoViewModel = new ViewModelProvider(requireActivity(), logtoViewModelFactory).get(LogtoViewModel.class);
+        logtoViewModel.isAuthenticated().observe(getViewLifecycleOwner(),  hasAuthenticated -> {
+            if (hasAuthenticated) {
+                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_tokenFragment);
+            }
+        });
+    }
+}
