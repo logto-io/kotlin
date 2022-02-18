a1ca30b7fa81fb5bd75b5c48b7b86d066938edd7
Xiao Yijun <xiaoyijun@silverhand.io>
2022-02-18 13:39:46 +0800

    feat(core): support request for raw string

--- kotlin-sdk/kotlin/src/main/kotlin/io/logto/sdk/core/http/HttpGet.kt
+++ kotlin-sdk/kotlin/src/main/kotlin/io/logto/sdk/core/http/HttpGet.kt
@@ -1,12 +1,40 @@
 package io.logto.sdk.core.http
 
-inline fun <reified T : Any> httpGet(uri: String, completion: HttpCompletion<T>) =
-    httpGet(uri, null, completion)
+import com.google.gson.JsonSyntaxException
+import io.logto.sdk.core.exception.ResponseException
 
 inline fun <reified T : Any> httpGet(
     uri: String,
-    headers: Map<String, String>? = null,
+    headers: Map<String, String>?,
     completion: HttpCompletion<T>,
-) = makeRequest(uri, null, headers, completion)
+) {
+    httpGet(uri, headers) httpRawGet@{ throwable, response ->
+        throwable?.let {
+            completion.onComplete(throwable, null)
+            return@httpRawGet
+        }
+        try {
+            completion.onComplete(null, gson.fromJson(response, T::class.java))
+        } catch (jsonSyntaxException: JsonSyntaxException) {
+            completion.onComplete(
+                ResponseException(ResponseException.Message.ERROR_RESPONSE, jsonSyntaxException),
+                null
+            )
+        }
+    }
+}
+
+inline fun <reified T : Any> httpGet(uri: String, completion: HttpCompletion<T>) =
+    httpGet(uri, null, completion)
 
 fun httpGet(uri: String, completion: HttpEmptyCompletion) = makeRequest(uri, null, null, completion)
+
+@JvmName("httpRawGet")
+fun httpGet(
+    uri: String,
+    headers: Map<String, String>?,
+    completion: HttpRawStringCompletion
+) = makeRequest(uri, null, headers, completion)
+
+@JvmName("httpRawGet")
+fun httpGet(uri: String, completion: HttpRawStringCompletion) = httpGet(uri, null, completion)
--- kotlin-sdk/kotlin/src/main/kotlin/io/logto/sdk/core/http/HttpPost.kt
+++ kotlin-sdk/kotlin/src/main/kotlin/io/logto/sdk/core/http/HttpPost.kt
@@ -1,22 +1,51 @@
 package io.logto.sdk.core.http
 
+import com.google.gson.JsonSyntaxException
+import io.logto.sdk.core.exception.ResponseException
 import okhttp3.RequestBody
 
 inline fun <reified T : Any> httpPost(
     uri: String,
     body: RequestBody,
+    headers: Map<String, String>?,
     completion: HttpCompletion<T>,
-) = httpPost(uri, body, null, completion)
+) {
+    httpPost(uri, body, headers) httpRawPost@{ throwable, response ->
+        throwable?.let {
+            completion.onComplete(throwable, null)
+            return@httpRawPost
+        }
+        try {
+            completion.onComplete(null, gson.fromJson(response, T::class.java))
+        } catch (jsonSyntaxException: JsonSyntaxException) {
+            completion.onComplete(
+                ResponseException(ResponseException.Message.ERROR_RESPONSE, jsonSyntaxException),
+                null
+            )
+        }
+    }
+}
 
-inline fun <reified T : Any> httpPost(
-    uri: String,
-    body: RequestBody,
-    headers: Map<String, String>? = null,
-    completion: HttpCompletion<T>
-) = makeRequest(uri, body, headers, completion)
+inline fun <reified T : Any> httpPost(uri: String, body: RequestBody, completion: HttpCompletion<T>) =
+    httpPost(uri, body, null, completion)
 
 fun httpPost(
     uri: String,
     body: RequestBody,
     completion: HttpEmptyCompletion,
+) = makeRequest(uri, body, null, completion)
+
+@JvmName("httpRawPost")
+fun httpPost(
+    uri: String,
+    body: RequestBody,
+    headers: Map<String, String>?,
+    completion: HttpRawStringCompletion
+) = makeRequest(uri, body, headers, completion)
+
+@JvmName("httpRawPost")
+fun httpPost(
+    uri: String,
+    body: RequestBody,
+    completion: HttpRawStringCompletion
 ) = makeRequest(uri, body, null, completion)
