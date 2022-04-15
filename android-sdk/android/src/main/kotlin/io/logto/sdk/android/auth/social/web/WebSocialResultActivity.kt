4ce8287f51598e202762c5f6bf730571fc2e216b
Xiao Yijun <xiaoyijun@silverhand.io>
2022-04-15 11:26:11 +0800

    feat(android-sdk): rename web social session key

--- android-sdk/android/src/main/kotlin/io/logto/sdk/android/auth/social/web/WebSocialResultActivity.kt
+++ android-sdk/android/src/main/kotlin/io/logto/sdk/android/auth/social/web/WebSocialResultActivity.kt
@@ -5,18 +5,18 @@ import android.os.Bundle
 
 class WebSocialResultActivity : Activity() {
     companion object {
-        private const val WEB_SOCIAL_CALLBACK_SCHEME = "logto-callback"
+        private const val WEB_SOCIAL_SESSION_KEY = "web"
         private val webSocialSession = mutableMapOf<String, WebSocialSession>()
 
         fun registerSession(session: WebSocialSession) {
-            webSocialSession[WEB_SOCIAL_CALLBACK_SCHEME] = session
+            webSocialSession[WEB_SOCIAL_SESSION_KEY] = session
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         intent.data?.let {
-            webSocialSession.remove(WEB_SOCIAL_CALLBACK_SCHEME)?.handleResult(it)
+            webSocialSession.remove(WEB_SOCIAL_SESSION_KEY)?.handleResult(it)
         }
         finish()
     }
