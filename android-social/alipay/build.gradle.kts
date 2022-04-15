5095aad1b2bcc9e4c65bbf8ba6f89a72d8603b5c
Xiao Yijun <xiaoyijun@silverhand.io>
2022-04-15 17:53:56 +0800

    feat(android-sdk): delete local aar file

--- android-social/alipay/build.gradle.kts
+++ android-social/alipay/build.gradle.kts
@@ -1,5 +1,14 @@
 group = "io.logto.sdk"
 version = libs.versions.logtoSdk.get()
 
+val alipaySdkName = "alipaySdk-15.7.9-20200727142846.aar"
+val sdk = file(alipaySdkName)
+if (!sdk.exists()) {
+    val sdkUri = "https://github.com/logto-io/social-sdks/raw/8aa90c6eaf68d1c4f2278bf7cd339b7a37c4c2c0/alipay/android/alipaySdk-15.7.9-20200727142846.aar"
+    uri(sdkUri).toURL().openStream().use {
+        it.copyTo(java.io.FileOutputStream(sdk))
+    }
+}
+
 configurations.create("default")
-artifacts.add("default", file("alipaySdk-15.7.9-20200727142846.aar"))
+artifacts.add("default", file(alipaySdkName))
