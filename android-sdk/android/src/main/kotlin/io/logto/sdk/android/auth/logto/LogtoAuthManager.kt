482658b920eeebf1d31203520c2aa86d6f7c1581
Xiao Yijun <xiaoyijun@silverhand.io>
2022-04-18 13:19:38 +0800

    refactor(android-sdk): logto auth manager

--- android-sdk/android/src/main/kotlin/io/logto/sdk/android/auth/logto/LogtoAuthManager.kt
+++ android-sdk/android/src/main/kotlin/io/logto/sdk/android/auth/logto/LogtoAuthManager.kt
@@ -1,27 +1,36 @@
 package io.logto.sdk.android.auth.logto
 
+import android.annotation.SuppressLint
 import android.net.Uri
 
 internal object LogtoAuthManager {
     private var logtoAuthCallbackUriScheme: String? = null
-    private val logtoAuthSession = mutableMapOf<String, LogtoAuthSession>()
+    @SuppressLint("StaticFieldLeak")
+    private var logtoAuthSession: LogtoAuthSession? = null
 
     fun handleAuthStart(scheme: String, authSession: LogtoAuthSession) {
-        logtoAuthSession[scheme] = authSession
+        logtoAuthSession = authSession
         logtoAuthCallbackUriScheme = scheme
     }
 
     fun handleCallbackUri(uri: Uri) {
-        logtoAuthSession.remove(uri.scheme)?.handleCallbackUri(uri)
+        logtoAuthSession?.handleCallbackUri(uri)
+        clearSession()
     }
 
     fun handleUserCancel() {
-        logtoAuthSession.remove(logtoAuthCallbackUriScheme)?.handleUserCancel()
+        logtoAuthSession?.handleUserCancel()
+        clearSession()
     }
 
     fun isLogtoAuthCallbackUriScheme(uriScheme: String?): Boolean {
         return uriScheme?.let {
-            logtoAuthSession.containsKey(it)
+            uriScheme == logtoAuthCallbackUriScheme
         } ?: false
+    }
+
+    private fun clearSession() {
+        logtoAuthSession = null
+        logtoAuthCallbackUriScheme = null
     }
 }
