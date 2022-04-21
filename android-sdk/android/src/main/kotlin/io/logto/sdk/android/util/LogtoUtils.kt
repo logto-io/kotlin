0c65f4c4e29c566e53b060bf706046d07d9e22c5
Xiao Yijun <xiaoyijun@silverhand.io>
2022-04-21 11:58:00 +0800

    test(android-sdk): add test for logto utils (#142)

--- android-sdk/android/src/main/kotlin/io/logto/sdk/android/util/LogtoUtils.kt
+++ android-sdk/android/src/main/kotlin/io/logto/sdk/android/util/LogtoUtils.kt
@@ -1,11 +1,12 @@
 package io.logto.sdk.android.util
 
+import java.util.Calendar
 import kotlin.math.floor
 
 object LogtoUtils {
     private const val MILLIS_PER_SECOND = 1000L
 
-    fun nowRoundToSec() = floor((System.currentTimeMillis() / MILLIS_PER_SECOND).toDouble()).toLong()
+    fun nowRoundToSec() = floor((Calendar.getInstance().timeInMillis / MILLIS_PER_SECOND).toDouble()).toLong()
 
     fun expiresAtFrom(startTime: Long, lifetime: Long): Long {
         return startTime + lifetime
