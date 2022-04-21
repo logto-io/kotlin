c16d386a3d81f6b1dbb4a4edb75b9bbe10c57e92
Xiao Yijun <xiaoyijun@silverhand.io>
2022-04-21 12:08:32 +0800

    test(android-sdk): add ut for logto auth session (#143)

--- android-sdk/android/build.gradle.kts
+++ android-sdk/android/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.browser)
 
     testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.robolectric)
     testImplementation(libs.truth)
     testImplementation(libs.mockk)
 }
