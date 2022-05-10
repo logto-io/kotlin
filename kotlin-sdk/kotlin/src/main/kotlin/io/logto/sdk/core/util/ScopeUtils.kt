54fcb6ae24933f8ea6b00d9d0ee8c2196e3ebbba
Xiao Yijun <xiaoyijun@silverhand.io>
2022-05-10 13:01:10 +0800

    docs: logto core utils

--- kotlin-sdk/kotlin/src/main/kotlin/io/logto/sdk/core/util/ScopeUtils.kt
+++ kotlin-sdk/kotlin/src/main/kotlin/io/logto/sdk/core/util/ScopeUtils.kt
@@ -3,6 +3,11 @@ package io.logto.sdk.core.util
 import io.logto.sdk.core.constant.ReservedScope
 
 object ScopeUtils {
+    /**
+     * Ensure the scope list contains `open_id` and `offline_access`
+     * @param[scopes] The origin scope list
+     * @return The scope list which contains `open_id` and `offline_access`
+     */
     fun withReservedScopes(scopes: List<String>?): List<String> =
         ((scopes ?: listOf()) + listOf(ReservedScope.OPENID, ReservedScope.OFFLINE_ACCESS)).distinct()
 }
